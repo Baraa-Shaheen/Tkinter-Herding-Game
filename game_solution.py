ef4394414a8033ce924accd2648f2aeca823b0b5
Baraa Shaheen <baraa.shaheen@student.manchester.ac.uk>
2023-11-19 19:33:55 +0000

    Added function for placing ball on canvas

--- game_solution.py
+++ game_solution.py
@@ -3,26 +3,31 @@
 import tkinter as tk
 
 class Ball:
-    def __init__(self, x, y, diameter, colour):
+    def __init__(self, radius, colour):
         self.x_speed = 0
         self.y_speed = 0
-        self.x = x
-        self.y = y
-        self.diameter = diameter 
+        self.radius = radius
         self.colour = colour
+        self.ball_id = None
     
-    def place_ball(canvas):
-        pass
-
+    def place_ball(self, canvas, x, y):
+        x1 = x - self.radius
+        y1 = y - self.radius
+        x2 = x + self.radius
+        y2 = y + self.radius
+        self.ball_id = canvas.create_oval(x1, y1, x2, y2, fill=self.colour)
 
 
 # Create 1920x1080 window with green canvas
 window = tk.Tk()
 window.geometry("1920x1080")
-canvas = tk.Canvas(window, width=1920, height=1080, bg="green")
+canvas_width = 1920
+canvas_height = 1080
+canvas = tk.Canvas(window, width=canvas_width, height=canvas_height, bg="green")
 canvas.pack()
 
-circle = canvas.create_oval(0,0,30,30, fill="black")
+player = Ball(15, "black")
+player.place_ball(canvas, canvas_width/2, canvas_height/2)
 
 
 
